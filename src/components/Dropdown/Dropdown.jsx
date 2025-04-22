3fce65b4ad2ec44cd4794a6159e3ecdfc696cab2
sanchousser <golyukaleks@gmail.com>
2025-04-22 18:16:56 +0200

    upd

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -2,11 +2,29 @@ import { Component } from "react";
 import css from './Dropdown.module.css'
 
 export class Dropdown extends Component {
+
+    state = {
+        visible: false,
+    }
+
+    toggleMenuBar = () => {
+        this.setState(({ visible }) => ({
+            visible: !visible
+        }))
+    }
+
     render() {
-        return(
+
+        return (
             <div className={css.dropdown}>
-                <button>Show</button>
-                <div className={css.menu}>menu</div>
+                <button onClick={this.toggleMenuBar}>
+                    {
+                        this.state.visible ? 'Hide' : 'Show'
+                    }
+                </button>
+                {
+                    this.state.visible && (<div className={css.menu}>menu</div>)
+                }
             </div>
         )
     }
